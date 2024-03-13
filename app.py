01369767b95c5189df5007312fac0ac2ae2fa1e8
Abdulrahman Tabaza <84684514+a-tabaza@users.noreply.github.com>
2024-03-14 02:28:26 +0300

    please work

--- app.py
+++ app.py
@@ -38,19 +38,19 @@ api_key = st.secrets["api_key"]
 
 embeddings_path = None
 index_path = None
-col4, col5 = st.columns(2)
 
 st.write("Select the type of embeddings to use:")
+col1, col2 = st.columns(2)
 
-with col4:
-    if st.button("Quantized Embeddings"):
-        embeddings_path = "embeddings_quantized.npy"
-        index_path = "likes_index_quantized.faiss"
-
-with col5:
+with col1:
     if st.button("float32 Embeddings"):
         embeddings_path = "embeddings_unquantized.npy"
         index_path = "likes_index_unquantized.faiss"
+
+with col2:
+    if st.button("Quantized Embeddings"):
+        embeddings_path = "embeddings_quantized.npy"
+        index_path = "likes_index_quantized.faiss"
 
 def query_lastfm(artist_name, track_name):
     res = requests.get(url = f"https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api_key}&artist={quote(artist_name.lower())}&track={quote(track_name.lower())}&format=json")
@@ -66,16 +66,16 @@ if embeddings_path and index_path:
         song_idx = np.random.choice(len(likes_dump), 16)
         songs = [likes_dump[i] for i in song_idx]
         for idx, song in enumerate(songs):
-            col1, col2 = st.columns(2)
+            col3, col4 = st.columns(2)
             metadata = query_lastfm(song['artist_name'], song['track_name'])
-            with col1:
+            with col3:
                 st.write("**Track:**", song['track_name'])
                 st.write("**Artist:**", song['artist_name'])
                 st.write("**Album:**", song['album_name'])
 
                 D, I = index.search(embeddings[song_idx[idx]].reshape(1,-1), 5)
                 
-            with col2:
+            with col4:
                 try:
                     st.image(metadata["track"]["album"]["image"][-1]["#text"])
                 except Exception as e:
@@ -84,12 +84,12 @@ if embeddings_path and index_path:
             st.audio(song['preview_url'])
             st.write("**Similar Tracks:**")
             for d, i in zip(D[0], I[0]):
-                col3, col4 = st.columns(2)
+                col5, col6 = st.columns(2)
                 if i != song_idx[idx]:
                     sim_metadata = query_lastfm(likes_dump[int(i)]['artist_name'], likes_dump[int(i)]['track_name'])
-                    with col3:
+                    with col5:
                         st.write(f"{likes_dump[int(i)]['track_name']} by {likes_dump[int(i)]['artist_name']}")
-                    with col4:
+                    with col6:
                         try:
                             st.image(sim_metadata["track"]["album"]["image"][-1]["#text"])
                         except Exception as e:
